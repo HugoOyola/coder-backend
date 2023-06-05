eae3e52bd06a64b525f92abce12f373ea676b984
Hugo Oyola <hugo.ham@outlook.com>
2023-06-05 18:56:32 -0500

    ✨ Login

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
 import socketProducts from './listeners/socketProducts.js';
 import registerChatHandler from './listeners/chatHandlers.js';
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import mongoStore from 'connect-mongo'
 
 import routerP from './routes/products.router.js';
 import routerC from './routes/carts.router.js';
 import routerV from './routes/views.router.js';
+import routerS from './routes/session.router.js';
 
 import __dirname from './utils.js';
 import connectToDB from './config/configServer.js';
@@ -25,9 +29,24 @@ app.set('view engine', 'handlebars');
 
 app.use('/api/products', routerP)
 app.use('/api/carts', routerC)
+app.use('/api/session', routerS)
 app.use('/', routerV);
 
 connectToDB()
+
+app.use(session({
+    store: mongoStore.create({
+        ttl:100000*60,
+        mongoUrl:'mongodb+srv://hugo29h:eN8xNDCYsEwjkaDL@e-commerce.vh8g3ls.mongodb.net/ecommerce?retryWrites=true&w=majority',
+        mongoOptions: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    }),
+    secret: 'HugoCoderBackEnd',
+    resave: false,
+    saveUninitialized: false
+}))
 
 const httpServer = app.listen(PORT, () => {
     try {
